3a74f1b1007f6fe0d142edd758b2b4e7a2570095
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-08-22 21:32:36 -0300

    Adicionando conteúdo aos arquivos JavaScript, HTML, e CSS.

---
+++ script.js
@@ -0,0 +1,11 @@
+function MostrarMenu() {
+    const MenuLateral = document.getElementById('Lateral')
+
+    if(MenuLateral.style.display = 'block') {
+        MenuLateral.style.display = 'none'
+    }
+
+    else {
+        MenuLateral.style.display = 'block'
+    }
+}
